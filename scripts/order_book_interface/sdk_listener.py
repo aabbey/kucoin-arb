d7c3d3f82ea1d45a357fcda8392be4139eec4b95
aabbey <alexabbey4@gmail.com>
2022-11-28 14:47:42 -0700

    got top 5 stream functional

--- scripts/order_book_interface/sdk_listener.py
+++ scripts/order_book_interface/sdk_listener.py
@@ -2,19 +2,21 @@ import asyncio
 from kucoin.client import WsToken
 from kucoin.ws_client import KucoinWsClient
 
-order_book_all = {"BTC-USDT", "ETH-USDT"}
+symbols_used = {"BTC-USDT", "ETH-USDT", "ETH-BTC"}
+order_book_all = dict.fromkeys(symbols_used)
 
 
 async def main():
     async def handle_msg(msg):
-        print(msg)
-
+        symbol = msg['topic'].split(':', 1)[1]
+        order_book_all[symbol] = msg['data']
     client = WsToken()
 
     ws_client = await KucoinWsClient.create(None, client, handle_msg, private=False)
-    await ws_client.subscribe('/spotMarket/level2Depth5:BTC-USDT,ETH-USDT')
+    await ws_client.subscribe('/spotMarket/level2Depth5:' + ','.join(symbols_used))
     while True:
-        await asyncio.sleep(5)
+        await asyncio.sleep(1)
+        print(order_book_all)
 
 
 if __name__ == "__main__":
