454382f756644259c57185ed1df77a9133294cb0
aabbey <alexabbey4@gmail.com>
2022-12-17 10:17:11 -0700

    made cycle finder

--- pre_processing/pre_process.py
+++ pre_processing/pre_process.py
@@ -1,8 +1,11 @@
 from kucoin.client import WsToken
 from kucoin.client import Market
 import os
+import numpy as np
 from pre_processing import constants
 
+
+CYCLE_LEN = 3
 
 
 def init_order_book():
@@ -20,7 +23,46 @@ def build_graph():
     pass
 
 
-def find_cycles(symbols):
-    cycles_with_symbol = {}
-    for s in symbols:
-        b, q = s.split('-')
+def append_next_currs(traj_in, adj_mat, cycle_over=False):
+    curr_in = traj_in[-1]
+    next_currs = np.where(adj_mat[curr_in] != 0)[0]
+    trajs_out = []
+    for c in next_currs:
+        if cycle_over:
+            if c == traj_in[0]:
+                trajs_out.append(traj_in + [c])
+        else:
+            if c not in traj_in:
+                trajs_out.append(traj_in + [c])
+
+    return trajs_out
+
+
+def find_cycles(adjacency_matrix):
+    cycle_indicies = []
+    adj_mat = adjacency_matrix.copy()
+    for start_curr in range(len(adj_mat[0]) - 2):
+        cycle_over_flag = False
+        all_trajs = [[0]]
+        new_trajs = append_next_currs(all_trajs[0], adj_mat)
+        for t in new_trajs:
+            all_trajs.append(t)
+        for level in range(CYCLE_LEN - 1):
+            if level == CYCLE_LEN - 2:
+                cycle_over_flag = True
+            for traj in all_trajs:
+                if len(traj) == 2 + level:
+                    new_trajs = append_next_currs(traj, adj_mat, cycle_over_flag)
+                    for t in new_trajs:
+                        all_trajs.append(t)
+
+        for l in all_trajs:
+            if len(l) == CYCLE_LEN + 1:
+                if start_curr > 0:
+                    cycle_indicies.append([x + start_curr for x in l])
+                else:
+                    cycle_indicies.append(l)
+
+        adj_mat = adj_mat[1:, 1:]
+
+    return np.array(cycle_indicies).T
